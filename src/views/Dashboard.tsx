13c5de22b87768fb6c720e360c95e80a533ba90b
Jason Healy <hello@jasonhealy.io>
2020-10-07 17:05:38 +0100

    style: dashboard activity loader

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -30,37 +30,47 @@ const Dashboard: React.FC<{}> = () => {
       </Heading>
       <Box marginTop={15}></Box>
       <Box paddingBottom={10}>
-        {messages?.map((message: any) => {
-          return (
-            <Box
-              borderRadius={5}
-              padding={10}
-              backgroundColor={'whitesmoke'}
-              paddingTop={20}
-              marginBottom={10}
-            >
-              <Box>
-                <Box
-                  marginBottom={10}
-                  display={'flex'}
-                  flexDirection={'row'}
-                  alignItems={'center'}
-                >
-                  <Text
-                    width={50}
-                    fontSize={12}
-                    fontWeight={'bold'}
-                    color={'darkgrey'}
-                    marginRight={20}
+        {messages ? (
+          messages?.map((message: any) => {
+            return (
+              <Box
+                borderRadius={5}
+                padding={10}
+                backgroundColor={'whitesmoke'}
+                paddingTop={20}
+                marginBottom={10}
+              >
+                <Box>
+                  <Box
+                    marginBottom={10}
+                    display={'flex'}
+                    flexDirection={'row'}
+                    alignItems={'center'}
                   >
-                    MESSAGE TYPE
-                  </Text>
-                  <Text fontSize={16}>{message.type}</Text>
+                    <Text
+                      width={50}
+                      fontSize={12}
+                      fontWeight={'bold'}
+                      color={'darkgrey'}
+                      marginRight={20}
+                    >
+                      MESSAGE TYPE
+                    </Text>
+                    <Text fontSize={16}>{message.type}</Text>
+                  </Box>
                 </Box>
               </Box>
+            )
+          })
+        ) : (
+          <Box>
+            <Box className={'spinner'}>
+              <Box className={'bounce1'}></Box>
+              <Box className={'bounce2'}></Box>
+              <Box className={'bounce3'}></Box>
             </Box>
-          )
-        })}
+          </Box>
+        )}
       </Box>
     </Box>
   )
